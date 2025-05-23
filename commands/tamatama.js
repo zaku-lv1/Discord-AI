2d1e9edd36b0fb85dd070e4a56c48c799c7c6e98
cui-cui-idv <z3505884@gmail.com>
2025-05-23 18:31:17 +0900

    tamatama

--- commands/tamatama.js
+++ commands/tamatama.js
@@ -12,17 +12,17 @@ module.exports = {
     const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }); // 使用するインテントを指定する
     await client.login(process.env.BOT_TOKEN); // BOT_TOKENはご自身のボットトークンに置き換えてください
     const user = await client.users.fetch(userId); // 指定されたユーザーIDを使用してユーザー情報を取得する
-    const kttWebhook = webhooks.find((webhook) => webhook.name === user.username);
+    const tamaWebhook = webhooks.find((webhook) => webhook.name === user.username);
 
-    if (kttWebhook) {
-      // KTTと井原先生が既に召喚されている場合は退出させる
-      await kttWebhook.delete(); // KTTのWebhookを削除する
+    if (tamaWebhook) {
+      // tamaが既に召喚されている場合は退出させる
+      await tamaWebhook.delete(); // tamaのWebhookを削除する
 
       const embed = new MessageEmbed()
         .setDescription('たまたまを退出させました。');
       await interaction.reply({ embeds: [embed] });
-    } else if (kttWebhook){
-      await kttWebhook.delete();
+    } else if (tamaWebhook){
+      await tamaWebhook.delete();
       const embed = new MessageEmbed()
         .setDescription('たまたまを退出させました。');
       await interaction.reply({ embeds: [embed] });
@@ -32,21 +32,17 @@ module.exports = {
         .setDescription('たまたまを退出させました。');
       await interaction.reply({ embeds: [embed] });
     } else {
-      // KTTと井原先生が召喚されていない場合は召喚する
-      const kttWebhook = await channel.createWebhook(user.username, {
+      // tamaが召喚されていない場合は召喚する
+      const tamaWebhook = await channel.createWebhook(user.username, {
         avatar: user.displayAvatarURL(), // ユーザーのアイコンを取得してWebhookのアイコンに設定する
       });
 
       const messages = [
-        '改心した？病気？どこかぶつけた？彼女できた？',
-        'きっっっしょ',
-        '可哀想www',
-        'わかる！！！！！！！',
-        '大丈夫！！？お大事に！！',
-        'はるかぜくんってかっこいいよね〜(棒)',
-        'ふーん',
-        'へぇ',
-        'りんちゃん一緒にかえろー',
+        'そうだよ',
+        '乞食だ！',
+        'これだから低身長は',
+        'ジャンプ買ってないやつは語んな',
+        '女あ'
       ];
 
       const filter = (message) => !message.author.bot;
@@ -56,18 +52,14 @@ module.exports = {
         const randomMessage = messages[Math.floor(Math.random() * messages.length)];
         const special_message = message.content;
 
-        if (special_message.includes('シュクダイ') || special_message.includes('宿題')) {
-          await kttWebhook.send('シュクダイ？ナニソレオイシイノ？');
-        } else if (special_message.includes('漢字')) {
-          await kttWebhook.send('漢字きもい！！！');
-        } else if (special_message.includes('天才')) {
-          await kttWebhook.send('え？待って泣ける嬉しい');
+        if (special_message.includes('TikTok')) {
+          await tamaWebhook.send('TikTokLITEか');
         }
       });
 
       collector.on('webhookUpdate', async () => {
         // Webhookが更新された場合の処理
-        await kttWebhook.delete(); // KTTのWebhookを削除する
+        await tamaWebhook.delete(); // tamaのWebhookを削除する
 
         const embed = new MessageEmbed()
           .setDescription('Webhookが更新されたため、たまたまを退出させました。');
